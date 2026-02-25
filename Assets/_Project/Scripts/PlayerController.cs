fe26c9c49b4fd7af587340927b4f6c06634e3360
yhr <danvalleribeiro@gmail.com>
2026-02-25 15:42:26 -0300

    Player screen movement and Clamp position to the Screen

--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -5,11 +5,19 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private PlayerInputReader playerInputReader;
     [SerializeField] private PlayerConfiguration playerConfig;
+    [SerializeField] private float xClampRange = 5f;
+    [SerializeField] private float yClampRange = 5f;
+
     private Vector2 _direction;
 
     private void Start()
     {
         playerInputReader.EnablePlayerInputActions();
+    }
+
+    private void Update()
+    {
+        TranslateBehaviour();
     }
 
     private void OnEnable()
@@ -25,6 +33,20 @@ public class PlayerController : MonoBehaviour
     private void OnMove(Vector2 move)
     {
         _direction = move;
-        Debug.Log(_direction * playerConfig.ShipSpeed);
+    }
+
+    private void TranslateBehaviour()
+    {
+        Vector3 rateChange = _direction * (playerConfig.ShipSpeed * Time.deltaTime);
+        transform.Translate(rateChange, Space.Self);
+        ClampPlayerPosition(rateChange);
+    }
+
+    private void ClampPlayerPosition(Vector3 rateChange) // using the camera probably is a more elegant way to do this.
+    {
+        Vector3 clampedPosition = transform.localPosition + rateChange;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -xClampRange, xClampRange);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -yClampRange, yClampRange);
+        transform.localPosition = new Vector3(clampedPosition.x, clampedPosition.y, transform.localPosition.z);
     }
 }
