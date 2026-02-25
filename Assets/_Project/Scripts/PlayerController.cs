21258f6a98c546fed87d15f6d94c00cfa9c7e618
yhr <danvalleribeiro@gmail.com>
2026-02-25 10:01:15 -0300

    Enemy Timeline, Assembly Definition Core and PlayerInput

---
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using PersonalPackage.Input;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private PlayerInputReader playerInputReader;
+    private Vector2 _direction;
+    
+    private void Start()
+    {
+        playerInputReader.EnablePlayerInputActions();
+    }
+        
+
+    private void OnEnable()
+    {
+        playerInputReader.Move += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        playerInputReader.Move -= OnMove;
+    }
+
+    private void OnMove(Vector2 move)
+    {
+        _direction = move;
+        Debug.Log(_direction);
+    }
+}
